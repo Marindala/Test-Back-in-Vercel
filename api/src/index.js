4bd0f45af2474bef020b85808def780299a1a322
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-11 23:34:27 -0300

    create .env

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,9 @@
-console.log("hello How are server")
+const express = require("express");
+require("dotenv").config();
+
+const app = express();
+const { PORT } = process.env;
+
+app.listen(PORT, () => {
+  console.log("%s listening at", PORT); // eslint-disable-line no-console
+});
