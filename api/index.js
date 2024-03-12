965b66b7297461f8949bf4de2d0b89a99393906d
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-12 01:22:20 -0300

    server

--- api/index.js
+++ api/index.js
@@ -1,9 +1,9 @@
-const app = require('./src/app.js');
+const server = require('./src/app.js');
 require("dotenv").config();
 
 
 const { PORT } = process.env;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log("%s listening at", PORT); // eslint-disable-line no-console
 });
