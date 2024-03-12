d501281e57280400c35027bdc2084ed30d0884a9
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-12 00:58:44 -0300

    building a back of 0

--- api/src/index.js
+++ api/src/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 require("dotenv").config();
-
+const morgan = require('morgan');
 const app = express();
+
+app.use(morgan('dev'));
+app.use(express.json())
+
+
 const { PORT } = process.env;
 
 app.listen(PORT, () => {
