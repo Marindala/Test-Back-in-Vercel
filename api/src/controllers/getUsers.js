ff52176174b9da49fab80c201ebd9dfd266dfbe9
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-13 21:47:41 -0300

    controllers

--- api/src/controllers/getUsers.js
+++ api/src/controllers/getUsers.js
@@ -4,7 +4,13 @@ let posteos = [];
 const getAllUsers = () => users;
 
 const getUserByName = (name) => {
-    const userFiltered = users.filter(user => user.name === name);
+    const userFiltered = users.filter(user => user.name === name); 
     if (userFiltered.length) return userFiltered;
-    return {error: `No hay usuarios con el nombre: ${name}`};
+    return {error: `No hay usuarios con el nombre: ${name}`}; 
+    //retorna un objeto con una propiedad error
 }
+
+module.exports = {
+    getAllUsers,
+    getUserByName
+}  
--- api/src/routes/userRouter.js
+++ api/src/routes/userRouter.js
@@ -1,8 +1,13 @@
 const { Router } = require('express');
+const {getAllUsers, getUserByName } = require ('../controllers/getUsers')
 const router = Router();
 
 
 router.get('/', (req, res) => {
+    const {name} = req.query;
+    if(name){
+        const user = getUserByName(name);
+    }
     
 })
 
