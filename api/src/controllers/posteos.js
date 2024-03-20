1c0c35b201facdfd154004133049def3078c7201
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-20 03:27:19 -0300

    push array posteos

--- api/src/controllers/posteos.js
+++ api/src/controllers/posteos.js
@@ -2,18 +2,13 @@ let posteos = [];
 
 let postId = 1;
 const postPosteos = (userId, title, content) => {
+  const newPost = {
+    userId,
+    title,
+    content,
+    id: postId++,
+  };
+  posteos.push(newPost);
+};
 
-    const newPost = {
-        userId,
-        title,
-        content,
-        id: postId++
-
-    }
-
-}
-
-
-module.exports = {
-
-}
+module.exports = {postPosteos};
