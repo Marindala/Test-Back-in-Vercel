0f0d336131c879d282248e6195131bfab8dfeb2e
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-20 03:24:10 -0300

    create postPosteos

--- api/src/controllers/posteos.js
+++ api/src/controllers/posteos.js
@@ -1,1 +1,19 @@
 let posteos = [];
+
+let postId = 1;
+const postPosteos = (userId, title, content) => {
+
+    const newPost = {
+        userId,
+        title,
+        content,
+        id: postId++
+
+    }
+
+}
+
+
+module.exports = {
+
+}
