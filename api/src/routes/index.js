4073d10fbfa5cf98b70ac3489dd90d3883894991
Marina <95050756+Marindala@users.noreply.github.com>
2024-03-20 03:12:26 -0300

    posteosRouter

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const router = Router();
 const userRouter = require ('./userRouter')
+const posteosRouter = require ('./posteosRouter')
 
 
 
 router.use('/users', userRouter)
+router.use('/posteos', posteosRouter)
 
 
 module.exports = router;
